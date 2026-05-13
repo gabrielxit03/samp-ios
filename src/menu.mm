149d5ac3b0da4684f34f798bd2224ac2fb7984fd
gabrielxit03 <124441718+gabrielxit03@users.noreply.github.com>
2026-05-12 23:12:04 -0300

    Update menu.mm

--- src/menu.mm
+++ src/menu.mm
@@ -23,151 +23,88 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Fundo com gradiente escuro
-    UIColor *corFundo = [UIColor colorWithRed:0.05 green:0.05 blue:0.08 alpha:1.0];
-    self.view.backgroundColor = corFundo;
-    
+    self.view.backgroundColor = [UIColor colorWithRed:0.05 green:0.05 blue:0.08 alpha:1.0];
     self.servidores = [NSMutableArray arrayWithArray:@[
-        @{@"nome":@"Brasil Roleplay",  @"ip":@"br-rp.com.br",  @"porta":@"7777", @"jogadores":@"100/500"},
-        @{@"nome":@"Server Teste",     @"ip":@"127.0.0.1",      @"porta":@"7777", @"jogadores":@"0/100"},
+        @{@"nome":@"Brasil RP Phoenix",  @"ip":@"brp2.phoenixhost.com.br", @"porta":@"7777", @"jogadores":@"0/500"},
+        @{@"nome":@"Brasil RP",          @"ip":@"brp.phoenixhost.com.br",  @"porta":@"7777", @"jogadores":@"0/500"},
+        @{@"nome":@"Servidor Teste",     @"ip":@"127.0.0.1",               @"porta":@"7777", @"jogadores":@"0/100"},
     ]];
-    
-    // Fechar teclado ao tocar fora
+
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
         initWithTarget:self action:@selector(fecharTeclado)];
     tap.cancelsTouchesInView = NO;
     [self.view addGestureRecognizer:tap];
-    
-    [self configurarInterface];
-    
-    // Observar teclado para mover a tela
+
     [[NSNotificationCenter defaultCenter] addObserver:self
         selector:@selector(tecladoAbriu:)
         name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self
         selector:@selector(tecladoFechou:)
         name:UIKeyboardWillHideNotification object:nil];
+
+    [self configurarInterface];
 }
 
-- (void)fecharTeclado {
-    [self.view endEditing:YES];
-}
+- (void)fecharTeclado { [self.view endEditing:YES]; }
 
 - (void)tecladoAbriu:(NSNotification*)n {
-    CGFloat altura = [n.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
-    [UIView animateWithDuration:0.3 animations:^{
-        self.view.transform = CGAffineTransformMakeTranslation(0, -altura * 0.4);
-    }];
+    CGFloat h = [n.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+    [UIView animateWithDuration:0.3 animations:^{ self.view.transform = CGAffineTransformMakeTranslation(0, -h*0.4); }];
 }
 
 - (void)tecladoFechou:(NSNotification*)n {
-    [UIView animateWithDuration:0.3 animations:^{
-        self.view.transform = CGAffineTransformIdentity;
-    }];
+    [UIView animateWithDuration:0.3 animations:^{ self.view.transform = CGAffineTransformIdentity; }];
 }
 
 - (void)configurarInterface {
     CGFloat w = self.view.bounds.size.width;
     CGFloat h = self.view.bounds.size.height;
-    CGFloat p = 20;
-    CGFloat y = 50;
+    CGFloat p = 20, y = 50;
 
-    // ── LOGO / TÍTULO ──────────────────────────
+    // Título
     UILabel *titulo = [UILabel new];
     titulo.text = @"SA-MP";
-    titulo.textColor = [UIColor colorWithRed:0.0 green:0.8 blue:0.2 alpha:1.0];
+    titulo.textColor = [UIColor colorWithRed:0.0 green:0.85 blue:0.2 alpha:1.0];
     titulo.font = [UIFont boldSystemFontOfSize:42];
     titulo.textAlignment = NSTextAlignmentCenter;
     titulo.frame = CGRectMake(0, y, w, 50);
     [self.view addSubview:titulo]; y += 45;
 
-    UILabel *subtitulo = [UILabel new];
-    subtitulo.text = @"San Andreas Multiplayer • iOS";
-    subtitulo.textColor = [UIColor colorWithWhite:0.5 alpha:1];
-    subtitulo.font = [UIFont systemFontOfSize:13];
-    subtitulo.textAlignment = NSTextAlignmentCenter;
-    subtitulo.frame = CGRectMake(0, y, w, 20);
-    [self.view addSubview:subtitulo]; y += 35;
+    UILabel *sub = [UILabel new];
+    sub.text = @"San Andreas Multiplayer • iOS";
+    sub.textColor = [UIColor colorWithWhite:0.4 alpha:1];
+    sub.font = [UIFont systemFontOfSize:13];
+    sub.textAlignment = NSTextAlignmentCenter;
+    sub.frame = CGRectMake(0, y, w, 20);
+    [self.view addSubview:sub]; y += 35;
 
-    // ── LINHA SEPARADORA ───────────────────────
-    UIView *linha1 = [UIView new];
-    linha1.backgroundColor = [UIColor colorWithRed:0.0 green:0.8 blue:0.2 alpha:0.3];
-    linha1.frame = CGRectMake(p, y, w-p*2, 1);
-    [self.view addSubview:linha1]; y += 20;
+    UIView *linha = [UIView new];
+    linha.backgroundColor = [UIColor colorWithRed:0.0 green:0.85 blue:0.2 alpha:0.3];
+    linha.frame = CGRectMake(p, y, w-p*2, 1);
+    [self.view addSubview:linha]; y += 20;
 
-    // ── CAMPO NOME ─────────────────────────────
-    UILabel *lNome = [UILabel new];
-    lNome.text = @"APELIDO";
-    lNome.textColor = [UIColor colorWithRed:0.0 green:0.8 blue:0.2 alpha:1.0];
-    lNome.font = [UIFont boldSystemFontOfSize:11];
-    lNome.frame = CGRectMake(p, y, w-p*2, 16);
-    [self.view addSubview:lNome]; y += 18;
-
-    self.campoNome = [UITextField new];
-    self.campoNome.placeholder = @"Digite seu apelido";
-    self.campoNome.attributedPlaceholder = [[NSAttributedString alloc]
-        initWithString:@"Digite seu apelido"
-        attributes:@{NSForegroundColorAttributeName: [UIColor colorWithWhite:0.35 alpha:1]}];
-    self.campoNome.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.15 alpha:1];
-    self.campoNome.textColor = UIColor.whiteColor;
-    self.campoNome.font = [UIFont systemFontOfSize:15];
-    self.campoNome.frame = CGRectMake(p, y, w-p*2, 44);
-    self.campoNome.layer.cornerRadius = 8;
-    self.campoNome.layer.borderWidth = 1;
-    self.campoNome.layer.borderColor = [UIColor colorWithWhite:0.2 alpha:1].CGColor;
-    self.campoNome.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,12,0)];
-    self.campoNome.leftViewMode = UITextFieldViewModeAlways;
-    self.campoNome.returnKeyType = UIReturnKeyDone;
+    // Campo apelido
+    [self addLabel:@"APELIDO" y:y w:w p:p];  y += 18;
+    self.campoNome = [self criarCampo:@"Digite seu apelido" y:y w:w p:p keyboard:UIKeyboardTypeDefault];
     [self.view addSubview:self.campoNome]; y += 52;
 
-    // ── IP E PORTA ─────────────────────────────
-    UILabel *lIP = [UILabel new];
-    lIP.text = @"ENDEREÇO DO SERVIDOR";
-    lIP.textColor = [UIColor colorWithRed:0.0 green:0.8 blue:0.2 alpha:1.0];
-    lIP.font = [UIFont boldSystemFontOfSize:11];
-    lIP.frame = CGRectMake(p, y, w-p*2, 16);
-    [self.view addSubview:lIP]; y += 18;
+    // Campos IP e Porta
+    [self addLabel:@"ENDEREÇO DO SERVIDOR" y:y w:w p:p]; y += 18;
+    CGFloat larguraIP = (w-p*2-10)*0.72;
+    CGFloat larguraPorta = (w-p*2-10)*0.28;
 
-    CGFloat larguraIP = (w - p*2 - 10) * 0.72;
-    CGFloat larguraPorta = (w - p*2 - 10) * 0.28;
-
-    self.campoIP = [UITextField new];
-    self.campoIP.placeholder = @"IP ou domínio";
-    self.campoIP.attributedPlaceholder = [[NSAttributedString alloc]
-        initWithString:@"IP ou domínio"
-        attributes:@{NSForegroundColorAttributeName: [UIColor colorWithWhite:0.35 alpha:1]}];
-    self.campoIP.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.15 alpha:1];
-    self.campoIP.textColor = UIColor.whiteColor;
-    self.campoIP.font = [UIFont systemFontOfSize:15];
-    self.campoIP.frame = CGRectMake(p, y, larguraIP, 44);
-    self.campoIP.layer.cornerRadius = 8;
-    self.campoIP.layer.borderWidth = 1;
-    self.campoIP.layer.borderColor = [UIColor colorWithWhite:0.2 alpha:1].CGColor;
-    self.campoIP.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,12,0)];
-    self.campoIP.leftViewMode = UITextFieldViewModeAlways;
-    self.campoIP.keyboardType = UIKeyboardTypeURL;
+    self.campoIP = [[UITextField alloc] initWithFrame:CGRectMake(p, y, larguraIP, 44)];
+    [self estilizarCampo:self.campoIP placeholder:@"IP ou domínio" keyboard:UIKeyboardTypeURL];
     self.campoIP.autocorrectionType = UITextAutocorrectionTypeNo;
     self.campoIP.autocapitalizationType = UITextAutocapitalizationTypeNone;
     [self.view addSubview:self.campoIP];
 
-    self.campoPorta = [UITextField new];
-    self.campoPorta.placeholder = @"7777";
-    self.campoPorta.attributedPlaceholder = [[NSAttributedString alloc]
-        initWithString:@"7777"
-        attributes:@{NSForegroundColorAttributeName: [UIColor colorWithWhite:0.35 alpha:1]}];
-    self.campoPorta.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.15 alpha:1];
-    self.campoPorta.textColor = UIColor.whiteColor;
-    self.campoPorta.font = [UIFont systemFontOfSize:15];
+    self.campoPorta = [[UITextField alloc] initWithFrame:CGRectMake(p+larguraIP+10, y, larguraPorta, 44)];
+    [self estilizarCampo:self.campoPorta placeholder:@"7777" keyboard:UIKeyboardTypeNumberPad];
     self.campoPorta.textAlignment = NSTextAlignmentCenter;
-    self.campoPorta.frame = CGRectMake(p + larguraIP + 10, y, larguraPorta, 44);
-    self.campoPorta.layer.cornerRadius = 8;
-    self.campoPorta.layer.borderWidth = 1;
-    self.campoPorta.layer.borderColor = [UIColor colorWithWhite:0.2 alpha:1].CGColor;
-    self.campoPorta.keyboardType = UIKeyboardTypeNumberPad;
     [self.view addSubview:self.campoPorta]; y += 52;
 
-    // ── BOTÃO CONECTAR ─────────────────────────
+    // Botão conectar
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     [btn setTitle:@"CONECTAR" forState:UIControlStateNormal];
     [btn setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
@@ -178,7 +115,7 @@
     [btn addTarget:self action:@selector(conectar) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn]; y += 60;
 
-    // ── LINHA SEPARADORA ───────────────────────
+    // Label servidores favoritos
     UILabel *lFav = [UILabel new];
     lFav.text = @"SERVIDORES FAVORITOS";
     lFav.textColor = [UIColor colorWithWhite:0.4 alpha:1];
@@ -186,16 +123,45 @@
     lFav.frame = CGRectMake(p, y, w-p*2, 16);
     [self.view addSubview:lFav]; y += 22;
 
-    // ── LISTA DE SERVIDORES ────────────────────
+    // Lista de servidores
     self.listaServidores = [[UITableView alloc]
-        initWithFrame:CGRectMake(0, y, w, h - y)
+        initWithFrame:CGRectMake(0, y, w, h-y)
                 style:UITableViewStylePlain];
     self.listaServidores.backgroundColor = UIColor.clearColor;
     self.listaServidores.delegate = self;
     self.listaServidores.dataSource = self;
     self.listaServidores.separatorColor = [UIColor colorWithWhite:0.12 alpha:1];
-    self.listaServidores.separatorInset = UIEdgeInsetsMake(0, p, 0, p);
     [self.view addSubview:self.listaServidores];
+}
+
+- (void)addLabel:(NSString*)texto y:(CGFloat)y w:(CGFloat)w p:(CGFloat)p {
+    UILabel *l = [UILabel new];
+    l.text = texto;
+    l.textColor = [UIColor colorWithRed:0.0 green:0.85 blue:0.2 alpha:1.0];
+    l.font = [UIFont boldSystemFontOfSize:11];
+    l.frame = CGRectMake(p, y, w-p*2, 16);
+    [self.view addSubview:l];
+}
+
+- (UITextField*)criarCampo:(NSString*)ph y:(CGFloat)y w:(CGFloat)w p:(CGFloat)p keyboard:(UIKeyboardType)kb {
+    UITextField *f = [[UITextField alloc] initWithFrame:CGRectMake(p, y, w-p*2, 44)];
+    [self estilizarCampo:f placeholder:ph keyboard:kb];
+    return f;
+}
+
+- (void)estilizarCampo:(UITextField*)f placeholder:(NSString*)ph keyboard:(UIKeyboardType)kb {
+    f.attributedPlaceholder = [[NSAttributedString alloc] initWithString:ph
+        attributes:@{NSForegroundColorAttributeName: [UIColor colorWithWhite:0.35 alpha:1]}];
+    f.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.15 alpha:1];
+    f.textColor = UIColor.whiteColor;
+    f.font = [UIFont systemFontOfSize:15];
+    f.layer.cornerRadius = 8;
+    f.layer.borderWidth = 1;
+    f.layer.borderColor = [UIColor colorWithWhite:0.2 alpha:1].CGColor;
+    f.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,12,0)];
+    f.leftViewMode = UITextFieldViewModeAlways;
+    f.keyboardType = kb;
+    f.returnKeyType = UIReturnKeyDone;
 }
 
 - (void)conectar {
@@ -213,12 +179,9 @@
 }
 
 - (void)alerta:(NSString*)msg {
-    UIAlertController *a = [UIAlertController
-        alertControllerWithTitle:@"SA-MP iOS"
-        message:msg
-        preferredStyle:UIAlertControllerStyleAlert];
-    [a addAction:[UIAlertAction actionWithTitle:@"OK"
-        style:UIAlertActionStyleDefault handler:nil]];
+    UIAlertController *a = [UIAlertController alertControllerWithTitle:@"SA-MP iOS"
+        message:msg preferredStyle:UIAlertControllerStyleAlert];
+    [a addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
     [self presentViewController:a animated:YES completion:nil];
 }
 
@@ -226,44 +189,32 @@
     return self.servidores.count;
 }
 
-- (UITableViewCell*)tableView:(UITableView*)tv cellForRowAtIndexPath:(NSIndexPath*)indexPath {
+- (UITableViewCell*)tableView:(UITableView*)tv cellForRowAtIndexPath:(NSIndexPath*)ip {
     UITableViewCell *cell = [tv dequeueReusableCellWithIdentifier:@"s"];
-    if (!cell) cell = [[UITableViewCell alloc]
-        initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"s"];
-
-    NSDictionary *s = self.servidores[indexPath.row];
+    if (!cell) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"s"];
+    NSDictionary *s = self.servidores[ip.row];
     cell.textLabel.text = s[@"nome"];
     cell.textLabel.textColor = UIColor.whiteColor;
     cell.textLabel.font = [UIFont boldSystemFontOfSize:14];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@:%@  •  %@ jogadores",
-        s[@"ip"], s[@"porta"], s[@"jogadores"]];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@:%@  •  %@ jogadores", s[@"ip"], s[@"porta"], s[@"jogadores"]];
     cell.detailTextLabel.textColor = [UIColor colorWithWhite:0.5 alpha:1];
     cell.detailTextLabel.font = [UIFont systemFontOfSize:12];
     cell.backgroundColor = [UIColor colorWithRed:0.08 green:0.08 blue:0.12 alpha:1];
-
-    // Ícone verde
     UIView *dot = [[UIView alloc] initWithFrame:CGRectMake(0,0,8,8)];
     dot.backgroundColor = [UIColor colorWithRed:0 green:0.85 blue:0.2 alpha:1];
     dot.layer.cornerRadius = 4;
     cell.accessoryView = dot;
-
     return cell;
 }
 
-- (void)tableView:(UITableView*)tv didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
-    NSDictionary *s = self.servidores[indexPath.row];
+- (void)tableView:(UITableView*)tv didSelectRowAtIndexPath:(NSIndexPath*)ip {
+    NSDictionary *s = self.servidores[ip.row];
     self.campoIP.text = s[@"ip"];
     self.campoPorta.text = s[@"porta"];
-    [tv deselectRowAtIndexPath:indexPath animated:YES];
+    [tv deselectRowAtIndexPath:ip animated:YES];
     [self fecharTeclado];
 }
 
-- (CGFloat)tableView:(UITableView*)tv heightForRowAtIndexPath:(NSIndexPath*)indexPath {
-    return 62;
-}
-
-- (UIView*)tableView:(UITableView*)tv viewForHeaderInSection:(NSInteger)section {
-    return nil;
-}
+- (CGFloat)tableView:(UITableView*)tv heightForRowAtIndexPath:(NSIndexPath*)ip { return 62; }
 
 @end
